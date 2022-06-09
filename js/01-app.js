caa019f123dd6c4079fb09356461e4471b22c458
miguelapol <apolonio1995@gmail.com>
2022-06-09 12:28:42 -0500

    viendo la clase patter

---
+++ js/01-app.js
@@ -0,0 +1,11 @@
+//class patter
+
+class Persona{
+    constructor(nombre,email){
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
+
+const persona = new Persona('Juan','apolonio1995@gmail.com');
+console.log(persona);
