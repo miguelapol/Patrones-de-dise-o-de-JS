6be4c84dab0f3ab865e69650f2865dd1f5abcec4
miguelapol <apolonio1995@gmail.com>
2022-06-10 13:07:05 -0500

    Añadiendo otra clase

--- js/06-app.js
+++ js/06-app.js
@@ -5,10 +5,16 @@ class Persona{
         this.email=email;
     }
 }
-
+class Cliente{
+    constructor(nombre,email,telefono){
+        this.nombre=nombre;
+        this.email=email;
+        this.telefono=telefono;
+    }
+}
 const funcionesPersona={
     mostrarInformation(){
-        console.log(`Nombre: ${this.nombre} Email: ${this.email}`);
+        console.log(`Nombre: ${this.nombre} Email: ${this.email} telefono: ${this.telefono}`);
     },
     mostrarNombre(){
         console.log(`Nombre: ${this.nombre}`);
@@ -17,8 +23,13 @@ const funcionesPersona={
 
 //añadir funcionesPersona a ala clase Persona 
 Object.assign(Persona.prototype,funcionesPersona);
+Object.assign(Cliente.prototype,funcionesPersona);
 
 const cliente= new Persona('Pedro','correo@gmail.com');
 console.log(cliente);
 cliente.mostrarInformation();
 cliente.mostrarNombre();
+
+const cliente2= new Cliente('Pedro','correo@gmail.com','123456789');
+console.log(cliente2);
+cliente2.mostrarInformation();
